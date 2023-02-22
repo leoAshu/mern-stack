808fa17cc1f650f5d18d759b81f7c2588a3003dd
leoAshu <ashutosh.ojha2009@gmail.com>
2023-02-22 15:32:35 -0800

    adds alternate way of adding event listeners in drum kit project

--- projects/drum-kit/scripts/index.js
+++ projects/drum-kit/scripts/index.js
@@ -1,43 +1,74 @@
-// function handleClick(b) {
-//     console.log(b);
-// }
+function handleClick() {
+
+    var audio;
+
+    switch (this.innerHTML) {
+        case "w":
+            audio = new Audio("./sounds/tom-1.mp3");
+            break;
+        case "a":
+            audio = new Audio("./sounds/tom-2.mp3");
+            break;
+        case "s":
+            audio = new Audio("./sounds/tom-3.mp3");
+            break;
+        case "d":
+            audio = new Audio("./sounds/tom-4.mp3");
+            break;
+        case "j":
+            audio = new Audio("./sounds/snare.mp3");
+            break;
+        case "k":
+            audio = new Audio("./sounds/crash.mp3");
+            break;
+        case "l":
+            audio = new Audio("./sounds/kick-bass.mp3");
+            break;
+        default:
+            console.log(this.innerHTML);
+            break;
+    }
+
+    audio.play();
+}
+
 
 var buttons = document.querySelectorAll(".drum");
 
 for(var i = 0; i < buttons.length; i++) {
-    // buttons[i].addEventListener("click", handleClick, this);
+    buttons[i].addEventListener("click", handleClick);
     
-    buttons[i].addEventListener("click", function () {
+    // buttons[i].addEventListener("click", function () {
         
-        var audio;
+    //     var audio;
 
-        switch (this.innerHTML) {
-            case "w":
-                audio = new Audio("./sounds/tom-1.mp3");
-                break;
-            case "a":
-                audio = new Audio("./sounds/tom-2.mp3");
-                break;
-            case "s":
-                audio = new Audio("./sounds/tom-3.mp3");
-                break;
-            case "d":
-                audio = new Audio("./sounds/tom-4.mp3");
-                break;
-            case "j":
-                audio = new Audio("./sounds/snare.mp3");
-                break;
-            case "k":
-                audio = new Audio("./sounds/crash.mp3");
-                break;
-            case "l":
-                audio = new Audio("./sounds/kick-bass.mp3");
-                break;
-            default:
-                console.log(this.innerHTML);
-                break;
-        }
+    //     switch (this.innerHTML) {
+    //         case "w":
+    //             audio = new Audio("./sounds/tom-1.mp3");
+    //             break;
+    //         case "a":
+    //             audio = new Audio("./sounds/tom-2.mp3");
+    //             break;
+    //         case "s":
+    //             audio = new Audio("./sounds/tom-3.mp3");
+    //             break;
+    //         case "d":
+    //             audio = new Audio("./sounds/tom-4.mp3");
+    //             break;
+    //         case "j":
+    //             audio = new Audio("./sounds/snare.mp3");
+    //             break;
+    //         case "k":
+    //             audio = new Audio("./sounds/crash.mp3");
+    //             break;
+    //         case "l":
+    //             audio = new Audio("./sounds/kick-bass.mp3");
+    //             break;
+    //         default:
+    //             console.log(this.innerHTML);
+    //             break;
+    //     }
 
-        audio.play();
-    });
+    //     audio.play();
+    // });
 }
