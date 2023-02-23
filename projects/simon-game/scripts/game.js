594958f3d4dba17cbc5eb7b04ba79126a82d7266
leoAshu <ashutosh.ojha2009@gmail.com>
2023-02-22 21:49:00 -0800

    plays sound on button click

--- projects/simon-game/scripts/game.js
+++ projects/simon-game/scripts/game.js
@@ -4,20 +4,26 @@ var gamePattern = [];
 var userClickedPattern = [];
 
 $(".btn").click(function() {
+
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
-  console.log(userClickedPattern);
+
+  playSound(userChosenColour);
 });
 
 function nextSequence() {
+
     var randomNumber = Math.floor(Math.random() * 4);
-
     var randomChosenColour = buttonColours[randomNumber];
-
     gamePattern.push(randomChosenColour);
 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    var audio = new Audio("./sounds/" + randomChosenColour + ".mp3");
+    playSound(randomChosenColour);
+}
+
+function playSound(name) {
+    
+    var audio = new Audio("./sounds/" + name + ".mp3");
     audio.play();
 }
