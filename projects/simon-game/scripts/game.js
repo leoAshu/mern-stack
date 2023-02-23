e5e9bd9658b3409d0d0d3f92b3127c910350a276
leoAshu <ashutosh.ojha2009@gmail.com>
2023-02-22 22:05:21 -0800

    implements game start on key down and level update

--- projects/simon-game/scripts/game.js
+++ projects/simon-game/scripts/game.js
@@ -3,6 +3,17 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 
+var started = false;
+var level = 0;
+
+$(document).keydown(function() {
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+      }
+});
+
 $(".btn").click(function() {
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
@@ -13,6 +24,9 @@ $(".btn").click(function() {
 });
 
 function nextSequence() {
+    level++;
+    $("#level-title").text("Level " + level);
+
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
