6d262af18536e62b88daff8e57f9432dc32b0ddf
leoAshu <ashutosh.ojha2009@gmail.com>
2023-02-22 22:24:59 -0800

    implements start over after wrong user click in simon game

--- projects/simon-game/scripts/game.js
+++ projects/simon-game/scripts/game.js
@@ -43,6 +43,8 @@ function checkAnswer(currentLevel) {
         }, 200);
 
         $("#level-title").text("Game Over, Press Any Key to Restart");
+
+        startOver();
     }
 }
 
@@ -72,4 +74,10 @@ function animatePress(currentColor) {
     setTimeout(function () {
       $("#" + currentColor).removeClass("pressed");
     }, 100);
+}
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
 }
