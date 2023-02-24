6a271ad9dec242d93871b907c27d4d3f310be743
leoAshu <ashutosh.ojha2009@gmail.com>
2023-02-23 22:28:55 -0800

    adds additional routes in my first express server project

--- modules/back-end/express-js/my-express-server/server.js
+++ modules/back-end/express-js/my-express-server/server.js
@@ -8,6 +8,14 @@ app.get("/", function (req, res) {
     res.send("<h1>Hello World!</h1>");
 });
 
+app.get("/contact", function (req, res) {
+    res.send("<h3>Contact me at: ashutosh.ojha2009@gmail.com</h3>");
+});
+
+app.get("/about", function (req, res) {
+    res.send("<p>I am Ashutosh and I love beer!</p>");
+});
+
 app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
