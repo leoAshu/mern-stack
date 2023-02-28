fd6c6cbb5d5fcec3a8fcb5c96f9b1ce39cbc8a3b
leoAshu <ashutosh.ojha2009@gmail.com>
2023-02-28 14:44:53 -0800

    sends success and failure pages as response based on result in newsletter signup project

--- projects/newsletter-signup/app.js
+++ projects/newsletter-signup/app.js
@@ -6,9 +6,6 @@ const app = express();
 app.use(express.static("public"));
 app.use(parser.urlencoded({extended: true}));
 
-const apiKey = "35426313c0e6a2758eb43545155e631e-us17";
-const listId = "e17434feb7";
-
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/signup.html");
 });
@@ -37,6 +34,8 @@ app.post("/", function (req, res) {
 
     const jsonData = JSON.stringify(data);
 
+    const apiKey = "35426313c0e6a2758eb43545155e631e-us17";
+    const listId = "e17434feb7";
     const url = "https://us17.api.mailchimp.com/3.0/lists/" + listId;
 
     const options = {
@@ -47,7 +46,12 @@ app.post("/", function (req, res) {
     const request = https.request(url, options, function (response) {
         response.on("data", function (data) {
             console.log(JSON.parse(data));
-            res.send(JSON.parse(data));
+
+            if(response.statusCode === 200) {
+                res.sendFile(__dirname + "/success.html");
+            } else {
+                res.sendFile(__dirname + "/failure.html");
+            }
         });
     });
 
