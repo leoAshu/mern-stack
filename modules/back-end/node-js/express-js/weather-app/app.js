f20e3d111b86622eccc85d6d5f68af6f5ea02a0e
leoAshu <ashutosh.ojha2009@gmail.com>
2023-02-27 21:16:20 -0800

    parses the response into a JSON object in weather project

--- modules/back-end/node-js/express-js/weather-app/app.js
+++ modules/back-end/node-js/express-js/weather-app/app.js
@@ -12,7 +12,12 @@ app.get("/", function (req, res) {
     const url = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=09dd9ff381005e3b324f253dd790f888";
 
     https.get(url, function (response) {
-        console.log(response);
+        console.log("statusCode:", response.statusCode);
+
+        response.on("data", function (data) {
+            const weatherData = JSON.parse(data);
+            console.log("responseData:", weatherData);
+        });
     });
 
     res.send("Server is up and running.");
