5cb39d24471cdf14f7c0ded26daeee9fa0dbf5b9
leoAshu <ashutosh.ojha2009@gmail.com>
2023-02-27 21:08:33 -0800

    uses https package to make get request to open weather api in weather project

---
+++ modules/back-end/node-js/express-js/weather-app/app.js
@@ -0,0 +1,23 @@
+//jshint version:6
+
+// 09dd9ff381005e3b324f253dd790f888
+// https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid={API key}
+
+const express = require("express");
+const https = require("https");
+
+const app = express();
+
+app.get("/", function (req, res) {
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=09dd9ff381005e3b324f253dd790f888";
+
+    https.get(url, function (response) {
+        console.log(response);
+    });
+
+    res.send("Server is up and running.");
+});
+
+app.listen(3000, function () {
+    console.log("Server is running on port 3000.");
+});
