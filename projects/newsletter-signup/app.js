4a7e359a07b8b03138620c138ae4b59e5dd1a5a0
leoAshu <ashutosh.ojha2009@gmail.com>
2023-02-28 13:25:53 -0800

    implements post route to read and log the inputs in newsletter signup project

--- projects/newsletter-signup/app.js
+++ projects/newsletter-signup/app.js
@@ -9,6 +9,15 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + "/signup.html");
 });
 
+app.post("/", function (req, res) {
+    const fName = req.body.firstName;
+    const lName = req.body.lastName;
+    const email = req.body.email;
+
+    console.log(fName + " " + lName);
+    console.log(email);
+});
+
 app.listen(3000, function () {
     console.log("Server is running on port 3000.");
 });
