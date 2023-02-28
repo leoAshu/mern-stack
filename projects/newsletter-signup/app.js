4daa12b45b56cc8ffd310a10fc5d52029bdb8f41
leoAshu <ashutosh.ojha2009@gmail.com>
2023-02-28 13:18:26 -0800

    implements express to use static folder to use css and assets folder in newsletter signup project

--- projects/newsletter-signup/app.js
+++ projects/newsletter-signup/app.js
@@ -2,7 +2,12 @@ const express = require("express");
 const parser = require("body-parser");
 
 const app = express();
+app.use(express.static("public"));
 app.use(parser.urlencoded({extended: true}));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
 
 app.listen(3000, function () {
     console.log("Server is running on port 3000.");
