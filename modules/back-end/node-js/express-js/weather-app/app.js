bf2cc7c765da14a4b6567d68ee35355473bceb99
leoAshu <ashutosh.ojha2009@gmail.com>
2023-02-27 21:39:32 -0800

    uses res.write to send multiple response texts in weather project

--- modules/back-end/node-js/express-js/weather-app/app.js
+++ modules/back-end/node-js/express-js/weather-app/app.js
@@ -9,18 +9,25 @@ const https = require("https");
 const app = express();
 
 app.get("/", function (req, res) {
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=09dd9ff381005e3b324f253dd790f888";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=San Jose,us&units=metric&appid=09dd9ff381005e3b324f253dd790f888";
 
     https.get(url, function (response) {
         console.log("statusCode:", response.statusCode);
 
         response.on("data", function (data) {
             const weatherData = JSON.parse(data);
-            console.log("responseData:", weatherData);
+            const temperature = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const iconURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            res.write("<p>The weather is currently " + weatherDescription + "</p>");
+            res.write("<h1>The temperature in San Jose is " + temperature + " degrees Celsius.</h1>");
+            res.write("<img src=" + iconURL + ">");
+            res.send();
         });
+
     });
 
-    res.send("Server is up and running.");
 });
 
 app.listen(3000, function () {
