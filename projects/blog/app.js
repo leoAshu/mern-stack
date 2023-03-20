3c670d420775d664ecc50e5b64776f11ead2c4f3
leoAshu <ashutosh.ojha2009@gmail.com>
2023-03-20 13:14:12 -0700

    blog: implements search post by title and converts all functions to async/await

--- projects/blog/app.js
+++ projects/blog/app.js
@@ -21,15 +21,16 @@ const homeStartingContent = 'Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = 'Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.';
 const contactContent = 'Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.';
 
-const posts = [];
-
-app.get('/', (req, res) => {
-  Post.find().then(posts => {
+app.get('/', async (req, res) => {
+  try {
+    const posts = await Post.find();
     res.render('home', {
       content: homeStartingContent,
       posts: posts
     });
-  })
+  } catch(err) {
+    console.error(err);
+  }
 });
 
 app.get('/about', (req, res) => {
@@ -44,25 +45,27 @@ app.get('/compose', (req, res) => {
   res.render('compose');
 });
 
-app.post('/compose', (req, res) => {
+app.post('/compose', async (req, res) => {
   const post = new Post({
     title: req.body.postTitle,
     content: req.body.postContent
   });
-  
-  post.save().then(() => {
+
+  try {
+    await post.save();
     res.redirect('/');
-  });
+  } catch(err) {
+    console.error(err);
+  }
 });
 
-app.get('/posts/:postName', (req, res) => {
-  const requestedTitle = req.params.postName;
-  
-  let flag = false;
-  posts.forEach(post => {
-    if(_.lowerCase(post.title) === _.lowerCase(requestedTitle))
-      res.render('post', {post: post});
-  });
+app.get('/posts/:postName', async (req, res) => {
+  try {
+    const post = await Post.findOne({title: req.params.postName});
+    res.render('post', {post: post});
+  } catch(err) {
+    console.error(err);
+  }
 });
 
 
