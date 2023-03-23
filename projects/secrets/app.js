5a96dd69528470882a729173a74428b792637a5d
leoAshu <ashutosh.ojha2009@gmail.com>
2023-03-23 14:44:18 -0700

    secrets: initial setup

--- projects/secrets/app.js
+++ projects/secrets/app.js
@@ -1,1 +1,14 @@
-//jshint esversion:6
+const express = require('express');
+const parser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(parser.urlencoded({extended: true}));
+
+
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000.');
+});
