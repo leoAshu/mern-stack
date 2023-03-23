70041d1733b079b5c7889e0f000fb26ff350e029
leoAshu <ashutosh.ojha2009@gmail.com>
2023-03-23 14:48:06 -0700

    secrets: implements get method to render pages

--- projects/secrets/app.js
+++ projects/secrets/app.js
@@ -7,7 +7,17 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(parser.urlencoded({extended: true}));
 
+app.get('/', (req, res) => {
+    res.render('home');
+});
 
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
 
 app.listen(3000, () => {
     console.log('Server started on port 3000.');
