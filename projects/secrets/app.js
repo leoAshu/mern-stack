e35ddb8dcc1eff3073a46be3052214df3116fb8f
leoAshu <ashutosh.ojha2009@gmail.com>
2023-03-23 15:06:04 -0700

    secrets: level 1 authentication using username and password only

--- projects/secrets/app.js
+++ projects/secrets/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const parser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(parser.urlencoded({extended: true}));
+
+mongoose.connect('mongodb://127.0.0.1:27017/userDB');
+
+const user_schema = {
+    email: String,
+    password: String
+};
+const User = mongoose.model('User', user_schema);
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -17,6 +26,33 @@ app.get('/login', (req, res) => {
 
 app.get('/register', (req, res) => {
     res.render('register');
+});
+
+app.post('/register', async (req, res) => {
+    try {
+        const user = new User({
+            email: req.body.username,
+            password: req.body.password
+        });
+    
+        await user.save();
+        res.render('secrets');
+    } catch(err) {
+        console.log(err);
+    }
+});
+
+app.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({email: req.body.username});
+        if(user) {
+            if(user.password == req.body.password) {
+                res.render('secrets');
+            }
+        }
+    } catch(err) {
+        console.log(err);
+    }
 });
 
 app.listen(3000, () => {
