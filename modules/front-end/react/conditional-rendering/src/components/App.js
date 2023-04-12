888d7f9f7fe5c37fe2351b9dd1e4066cddfe601e
leoAshu <ashutosh.ojha2009@gmail.com>
2023-04-12 16:59:10 -0700

    conditional-rendering: seperates components login and input

--- modules/front-end/react/conditional-rendering/src/components/App.js
+++ modules/front-end/react/conditional-rendering/src/components/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import Login from './Login';
 
 function App() {
   return (
     <div className='container'>
       <h1>Hello</h1>
-      <form className='form'>
+      <Login />
+      {/* <form className='form'>
         <input type='text' placeholder='Username' />
         <input type='password' placeholder='Password' />
         <button type='submit'>Login</button>
-      </form>
+      </form> */}
     </div>
   );
 }
