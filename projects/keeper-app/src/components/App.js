6f5c70907e0c1d99c74ae1a7677280751e7f6462
leoAshu <ashutosh.ojha2009@gmail.com>
2023-04-12 16:41:07 -0700

    keeper-app: uses arrow function to clean code

--- projects/keeper-app/src/components/App.js
+++ projects/keeper-app/src/components/App.js
@@ -4,14 +4,6 @@ import Footer from './Footer';
 import Note from './Note';
 import notes from '../notes';
 
-function createNote(note) {
-    return <Note 
-        key={note.key}
-        title={note.title}
-        content={note.content}
-    />;
-}
-
 function App() {
     return (
         <div>
