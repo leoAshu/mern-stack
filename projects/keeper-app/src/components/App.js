1116638e552108ada4c36abe2e2ea34a76c647bd
leoAshu <ashutosh.ojha2009@gmail.com>
2023-04-12 16:36:32 -0700

    keeper-app: use props and map function to dynamically create notes

--- projects/keeper-app/src/components/App.js
+++ projects/keeper-app/src/components/App.js
@@ -1,13 +1,21 @@
-import React from "react-dom";
+import React from 'react-dom';
 import Header from './Header';
-import Footer from "./Footer";
-import Note from "./Note";
+import Footer from './Footer';
+import Note from './Note';
+import notes from '../notes';
+
+function createNote(note) {
+    return <Note 
+        title={note.title}
+        content={note.content}
+    />;
+}
 
 function App() {
     return (
         <div>
             <Header />
-            <Note />
+            {notes.map(createNote)}
             <Footer />
         </div>
     );
