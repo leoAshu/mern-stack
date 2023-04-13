d554d16b990952949c74d868e7381f4c2a3e98b6
leoAshu <ashutosh.ojha2009@gmail.com>
2023-04-13 15:49:20 -0700

    react-hooks: solves challenge problem to display time

--- modules/front-end/react/react-hooks/src/components/App.js
+++ modules/front-end/react/react-hooks/src/components/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 
 function App() {
     const [count, setCount] = useState(0);
+    const [time, setTime] = useState(new Date().toLocaleTimeString());
 
     function increment() {
         setCount(count + 1);
@@ -11,8 +12,14 @@ function App() {
         count > 0 && setCount(count - 1);
     }
 
+    function getTime() {
+        setTime(new Date().toLocaleTimeString());
+    }
+    setInterval(getTime, 1000);
+
     return (
         <div className='container'>
+            <h2>{time}</h2>
             <h1>{count}</h1>
             <button onClick={increment}>+</button>
             <button onClick={decrement}>-</button>
