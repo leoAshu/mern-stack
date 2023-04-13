7997b134051cfb9f44e4c4f7c252149618addaa3
leoAshu <ashutosh.ojha2009@gmail.com>
2023-04-13 15:35:10 -0700

    react-hooks: solves challenge to decrement counter value

--- modules/front-end/react/react-hooks/src/components/App.js
+++ modules/front-end/react/react-hooks/src/components/App.js
@@ -4,13 +4,18 @@ function App() {
     const [count, setCount] = useState(0);
 
     function increment() {
-        setCount(count+1);
+        setCount(count + 1);
+    }
+
+    function decrement() {
+        count > 0 && setCount(count - 1);
     }
 
     return (
         <div className="container">
             <h1>{count}</h1>
             <button onClick={increment}>+</button>
+            <button onClick={decrement}>-</button>
         </div>
     );
 }
