3b0e299454b3389b660f4cbdd2ad7bca374cfe54
leoAshu <ashutosh.ojha2009@gmail.com>
2023-04-13 23:11:05 -0700

    react-event-handling: solution to the challenge

--- modules/front-end/react/react-event-handling/src/components/App.js
+++ modules/front-end/react/react-event-handling/src/components/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function App() {
+  const [headingText, setHeadingText] = useState('Hello');
+  const [isMouseOver, setMouseOver] = useState(false);
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
+
+  function handleClick() {
+    setHeadingText('Submitted');
+  }
+
   return (
     <div className='container'>
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type='text' placeholder="What's your name?" />
-      <button>Submit</button>
+      <button 
+        style={{backgroundColor: isMouseOver ? 'black' : 'white'}} 
+        onMouseOver={handleMouseOver} 
+        onMouseOut={handleMouseOut} 
+        onClick={handleClick}>Submit</button>
     </div>
   );
 }
