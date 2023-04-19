c584d63fe89998066c26e3aee2cef5f1ff2ac44e
leoAshu <ashutosh.ojha2009@gmail.com>
2023-04-18 21:22:51 -0700

    keeper-app: updates CreateArea to manage input values locally

--- projects/keeper-app/src/components/CreateArea.js
+++ projects/keeper-app/src/components/CreateArea.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function CreateArea() {
-  return (
-    <div>
-      <form>
-        <input name='title' placeholder='Title' />
-        <textarea name='content' placeholder='Take a note...' rows='3' />
-        <button>Add</button>
-      </form>
-    </div>
-  );
+    const [note, setNote] = useState({title: '', content: ''});
+
+    function handleChange(event) {
+        const {name: eventName, value} = event.target;
+        setNote((prevNote) => {
+            return {
+                ...prevNote,
+                [eventName]: value
+            }
+        });
+    }
+
+    return (
+        <div>
+            <form>
+            <input 
+                onChange={handleChange} 
+                name='title' 
+                placeholder='Title' 
+                value={note.title}
+            />
+            <textarea 
+                onChange={handleChange} 
+                name='content' 
+                placeholder='Take a note...' 
+                rows='3' 
+                value={note.content}
+            />
+            <button>Add</button>
+            </form>
+        </div>
+    );
 }
 
 export default CreateArea;
