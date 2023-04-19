ca75d232b85ad80ae63c167b9d121076088bdfb0
leoAshu <ashutosh.ojha2009@gmail.com>
2023-04-18 17:28:51 -0700

    react-complex-state: simple solution

--- modules/front-end/react/react-complex-state-modification/src/components/App.js
+++ modules/front-end/react/react-complex-state-modification/src/components/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [fName, setFName] = useState('');
+  const [lName, setLName] = useState('');
+
+  function onChangeFName(event) {
+    setFName(event.target.value);
+  }
+
+  function onChangeLName(event) {
+    setLName(event.target.value);
+  }
 
   return (
     <div className='container'>
       <h1>
-        Hello
+        Hello {fName} {lName}
       </h1>
       <form>
-        <input name='fName' placeholder='First Name' />
-        <input name='lName' placeholder='Last Name' />
+        <input onChange={onChangeFName} name='fName' placeholder='First Name' />
+        <input onChange={onChangeLName} name='lName' placeholder='Last Name' />
         <button>Submit</button>
       </form>
     </div>
