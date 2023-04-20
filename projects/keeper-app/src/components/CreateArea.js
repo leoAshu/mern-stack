5369933ca2b934f371876b1166789a5d1f7f40a1
leoAshu <ashutosh.ojha2009@gmail.com>
2023-04-19 17:45:27 -0700

    keeper-app: finishes app by styling using material ui

--- projects/keeper-app/src/components/CreateArea.js
+++ projects/keeper-app/src/components/CreateArea.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
+import AddIcon from '@mui/icons-material/Add';
+import { Fab } from '@mui/material';
+import { Zoom } from '@mui/material';
 
 function CreateArea(props) {
+    const [isExpanded, setExpanded] = useState(false);
+
     const [note, setNote] = useState({
         title: '', 
         content: ''
@@ -26,25 +31,33 @@ function CreateArea(props) {
         event.preventDefault();
     }
 
+    function expand() {
+        setExpanded(true);
+    }
+
     return (
         <div>
-            <form 
-                className='create-note'
-                onSubmit={submitNote}>
-                <input 
-                    onChange={handleChange} 
-                    name='title' 
-                    placeholder='Title' 
-                    value={note.title}
-                />
+            <form className='create-note'>
+                {isExpanded && (
+                    <input 
+                        onChange={handleChange} 
+                        name='title' 
+                        placeholder='Title' 
+                        value={note.title}
+                    />
+                )}
+                
                 <textarea 
+                    onClick={expand}
                     onChange={handleChange} 
                     name='content' 
                     placeholder='Take a note...' 
-                    rows='3' 
+                    rows={isExpanded? '3': '1' }
                     value={note.content}
                 />
-                <button>Add</button>
+                <Zoom in={isExpanded}>
+                <Fab onClick={submitNote}><AddIcon /></Fab>
+                </Zoom>
             </form>
         </div>
     );
